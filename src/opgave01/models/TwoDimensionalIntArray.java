60a339a92ccb1f29a6c958c3ccf7f4080ee98f39
Noah Kristensen <noahkristensen1@gmail.com>
2024-02-22 07:50:55 +0100

    opg1

--- src/opgave01/models/TwoDimensionalIntArray.java
+++ src/opgave01/models/TwoDimensionalIntArray.java
@@ -10,27 +10,46 @@ public class TwoDimensionalIntArray {
     }
 
     public void setValueAt(int row, int column, int value) {
-
+        intArray[row][column] = value;
     }
 
     public int getValueAt(int row, int column) {
-        return -1;
+        return intArray[row][column];
     }
 
 
     public int sumRow(int row) {
-        return -1;
+        int sum = 0;
+        for (int i = 0; i < intArray[row].length; i++) {
+            sum += intArray[row][i];
+        }
+        return sum;
     }
 
     public int sumColumn(int column) {
-        return -1;
+        int sum = 0;
+        for (int[] rows : intArray) {
+            sum += rows[column];
+        }
+        return sum;
     }
 
     public int sumAll() {
-        return -1;
+        int sum = 0;
+        for (int[] ints : intArray) {
+            for (int anint : ints) {
+                sum += anint;
+            }
+        }
+        return sum;
     }
 
     public void print() {
+        for (int[] ints : intArray) {
+            for (int anint : ints) {
+                System.out.printf("%4d",anint);
+            }
+        }
 
     }
 
